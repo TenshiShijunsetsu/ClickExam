f25ccdf499e59981626e530ce162b987d254f8e0
Anne <Anne@ANNE-PC>
2017-07-07 17:06:46 +0800

    Add .gitignore and .gitattributes.

---
+++ ClickExam/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClickExam.Models;
+
+namespace ClickExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ClikDBContext db = new ClikDBContext();
+
+        // GET: data from db
+        public ActionResult Index()
+        {
+            var cliks = from c in db.Cliks
+                        where c.ID == 1
+                        select c;
+            return View(cliks.First());
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(Clik clk)
+        {
+            // update db
+            if (clk.ClikCtr + 1 < 11)
+            {
+                var udb = db.Cliks.Find(clk.ID);
+                udb.ClikCtr = clk.ClikCtr + 1;
+
+                db.SaveChanges();
+            }
+
+            //display db data
+            ModelState.Clear();
+            var cliks = from c in db.Cliks
+                        where c.ID == clk.ID
+                        select c;
+            return View(cliks.First());
+        }
+    }
+}
---
+++ ClickExam/Models/Clik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ClickExam.Models
+{
+    public class Clik
+    {
+        public int ID { get; set; }
+        public int ClikCtr { get; set; }
+
+    }
+
+    public class ClikDBContext : DbContext
+    {
+        public ClikDBContext()
+        {
+
+        }
+        public DbSet<Clik> Cliks { get; set; }
+    }
+}
